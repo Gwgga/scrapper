28157d8c7aafda3ff718eaf5cc58645efcb2cb4f
Gustavo Araújo <gwga@hotmail.com>
2024-10-02 17:22:21 -0300

    Update browser.py

--- app/internal/browser.py
+++ app/internal/browser.py
@@ -95,6 +95,8 @@ async def page_processing(
     browser_params: BrowserQueryParams,
     init_scripts: Sequence[str] = None,
 ):
+    page.on("console", lambda msg: print(f"Console log: {msg.text()}"))
+    
     # add browser cookies before page load, if provided
     if params.cookies:
         for cookie in params.cookies:
